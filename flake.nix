03a1911ef583ea4a5a705b72573b162c4876cbce
Michael Harris <osiris2918@gmail.com>
2025-05-10 15:48:39 -0500

    add flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  # Useful tool to find specific nix revisions if you need old packages:
+  # https://lazamar.co.uk/nix-versions/
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = with pkgs;
+          mkShell {
+            buildInputs = [
+              pkgs.nodejs_22
+            ];
+
+            shellHook = ''
+            '';
+          };
+      });
+}
